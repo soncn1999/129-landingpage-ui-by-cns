dd67b60e84a16c1e1652435b2e6dd2695c44744a
soncn1999 <shasacaoson1999@gmail.com>
2023-07-27 11:51:52 +0700

    commit 1st demo

--- main.js
+++ main.js
@@ -9,11 +9,14 @@ $(document).ready(function () {
                 items: 1
             },
             600: {
-                items: 2
+                items: 1
             },
             1000: {
                 items: 3
-            }
+            },
+            1800: {
+                items: 3
+            },
         }
     });
 
@@ -30,6 +33,12 @@ $(document).ready(function () {
                 items: 1
             },
             1000: {
+                items: 1
+            },
+            2000: {
+                items: 1
+            },
+            2900: {
                 items: 1
             }
         }
